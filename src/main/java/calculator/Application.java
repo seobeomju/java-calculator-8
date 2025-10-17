dda37a771648f31d39e4d7839396cdc797c67199
seobeomju <alt10063@naver.com>
2025-10-17 22:08:24 +0900

    feat: 기본 구분자(, :)로 숫자 분리 후 합계 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,19 +3,36 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 
+
 public class Application {
+
+    private static final String BASIC_SEPARATOR = "[,:]";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해주세요");
+        System.out.println("덧셈할 문자열을 입력해 주세요:");
         String input = Console.readLine();
 
-        if (!CheckString(input)) {
+        if (!checkString(input)) {
             System.out.println("결과 : 0");
         }
+
+        int result = calculator(input);
+        System.out.println("결과 : " + result);
+
     }
 
-    public static boolean CheckString(String input) {
+    public static boolean checkString(String input) {
         return input != null && !input.isBlank();
+    }
+
+    public static int calculator(String input) {
+        int sum = 0;
+        String[] tokens = input.split(BASIC_SEPARATOR);
+        for (String token : tokens){
+            sum += Integer.parseInt(token);
+        }
+        return sum;
     }
 
 }
