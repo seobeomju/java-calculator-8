07c61b76e7b4e18d4b08a20efa5475ef0243848e
seobeomju <alt10063@naver.com>
2025-10-18 19:07:44 +0900

    feat: 빈 토큰, 숫자 체크, 잘못된 커스텀 헤더 예외처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,18 +13,18 @@ public class Application {
     public static void main(String[] args) {
 
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요:");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         //입력 내 \n을 실제 개행으로 변환
-        input = input.replace("\\n","\n");
+        input = input.replace("\\n", "\n");
 
         if (!checkString(input)) {
             System.out.println("결과 : 0");
             return;
         }
 
-        System.out.println("결과 : " + inputParser(input));
+        System.out.println("결과 : " + parseAndCalculate(input));
 
     }
 
@@ -33,29 +33,50 @@ public class Application {
 
     }
 
-    public static int inputParser(String input) {
+    public static int parseAndCalculate(String input) {
         //이유: //(0,1) + 구분자 1글자(2) + 개행(3)
-        if(input.startsWith(CUSTOM_SEPARATOR_START) && input.indexOf(CUSTOM_SEPARATOR_END) == 3) {
-            String delimiter = input.substring(2,3);
-            String body = input.substring(4);
+        if (input.startsWith(CUSTOM_SEPARATOR_START)) {
+            int end = input.indexOf(CUSTOM_SEPARATOR_END);
+            if (end != 3) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
+            }
+
+            String delimiter = input.substring(2, 3);
+            String body = input.substring(end + 1);
 
             //메타 문자 안전 처리
             String splitRegex = Pattern.quote(delimiter);
-            return calculator(body,splitRegex);
+            return calculator(body, splitRegex);
         }
 
-        return calculator(input,BASIC_SEPARATOR);
+        return calculator(input, BASIC_SEPARATOR);
     }
 
-    public static int calculator(String input, String delimiter) {
+    public static int calculator(String input, String splitRegex) {
         int sum = 0;
-        String[] tokens = input.split(delimiter);
-        for (String token : tokens){
+        // 끝 토큰까지 확인
+        String[] tokens = input.split(splitRegex, -1);
+        for (String token : tokens) {
             String number = token.trim();
+
+            validate(number);
+
             sum += Integer.parseInt(number);
         }
         return sum;
     }
 
+    public static void validate(String input) {
+
+        //빈 토큰 체크
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 형식: 빈 값이 포함되어 있습니다.");
+        }
+
+        //숫자 체크
+        if (!input.matches("^[1-9]\\d*$")) {
+            throw new IllegalArgumentException("숫자는 양수만 가능합니다.");
+        }
+    }
 
 }
