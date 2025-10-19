0ebc3ad157b4ef20f8f382af10764ab2a26eb165
seobeomju <alt10063@naver.com>
2025-10-19 20:46:17 +0900

    refactor: StringCalculator 커스텀 분기 분리 및 입력 정규화

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -8,22 +8,28 @@ public class StringCalculator {
     private static final String CUSTOM_SEPARATOR_END = "\n";
 
     public static int parseAndCalculate(String input) {
-        if (input == null || input.isBlank()) return 0;
+        if (input == null || input.isBlank()) {
+            return 0;
+        }
 
         //이유: //(0,1) + 구분자 1글자(2) + 개행(3)
         if (input.startsWith(CUSTOM_SEPARATOR_START)) {
-            int end = input.indexOf(CUSTOM_SEPARATOR_END);
-            if (end != 3) {
-                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
-            }
-
-            String delimiter = input.substring(2, 3);
-            String body = input.substring(end + 1);
-            //메타 문자 안전 처리
-            String splitRegex = Pattern.quote(delimiter);
-            return Calculator.calculator(body, splitRegex);
+            return calculatorWithCustom(input);
         }
 
         return Calculator.calculator(input, BASIC_SEPARATOR);
+    }
+
+    private static int calculatorWithCustom(String input){
+        final int end = input.indexOf(CUSTOM_SEPARATOR_END);
+        if (end != 3) {
+            throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
+        }
+
+        String delimiter = input.substring(2, 3);
+        String body = input.substring(end + 1);
+        //메타 문자 안전 처리
+        String splitRegex = Pattern.quote(delimiter);
+        return Calculator.calculator(body, splitRegex);
     }
 }
